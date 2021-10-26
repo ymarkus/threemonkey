6f8bc80ae58009f1fb4c51e7107cd3458f678709
Yannick Markus <ym@ymarkus.dev>
2021-10-26 13:41:22 +0200

    npm & tailwindcss files

---
+++ tailwind.config.js
@@ -0,0 +1,48 @@
+module.exports = {
+  purge: [
+    './**/*.html',
+    './**/*.js',
+    './**/*.css',
+    './**/*.twig'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      backgroundSize: {
+        'full': '100% 100%',
+      },
+      minHeight: {
+        'screen-1/2': '50vh',
+        'screen-3/5': '60vh',
+        'screen-3/4': '75vh',
+        'screen-4/5': '80vh',
+      },
+      maxHeight: {
+        'screen-1/2': '50vh',
+        'screen-3/5': '60vh',
+        'screen-3/4': '75vh',
+        'screen-4/5': '80vh',
+      },
+      minWidth: {
+        'screen-1/2': '50vw',
+        'screen-3/5': '60vw',
+        'screen-3/4': '75vw',
+        'screen-4/5': '80vw',
+      },
+      maxWidth: {
+        'screen-1/2': '50vw',
+        'screen-3/5': '60vw',
+        'screen-3/4': '75vw',
+        'screen-4/5': '80vw',
+      },
+      zIndex: {
+        '-10': '-10'
+      },
+      transform: ['hover', 'focus']
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
