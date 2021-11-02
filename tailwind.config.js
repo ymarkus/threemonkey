6ea5b5d06d4f2680c55e17e53137e19dcf80853b
Yannick Markus <ym@ymarkus.dev>
2021-11-02 18:05:46 +0100

    add record

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
   theme: {
     extend: {
       minHeight: {
+        'screen-1/5': '20vh',
+        'screen-1/4': '25vh',
+        'screen-1/3': '33.333333vh',
         'screen-1/2': '50vh',
         'screen-4/5': '80vh',
         'screen-9/10': '90vh',
